39dc6965cc22b149aeae1076c2f6e3a151bb3633
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:50:28 +0100

    .

---
+++ _sunamo/SH.cs
@@ -0,0 +1,11 @@
+
+namespace SunamoPlatformUwpInterop._sunamo;
+
+internal class SH
+{
+    //internal static Func<string, char, string> RemoveAfterFirstChar;
+    internal static string RemoveAfterFirstChar(string name, char dot)
+    {
+        return name;
+    }
+}
