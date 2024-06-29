f8709d081c76704709fb93582b4c943e4a422672
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:43 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoFileSystem/FSND.cs
@@ -0,0 +1,97 @@
+namespace SunamoPlatformUwpInterop;
+
+//namespace SunamoPlatformUwpInterop;
+
+internal class FSND
+{
+    internal static void CreateUpfoldersPsysicallyUnlessThere(string nad)
+    {
+        CreateFoldersPsysicallyUnlessThere(Path.GetDirectoryName(nad));
+    }
+
+    internal static void CreateFoldersPsysicallyUnlessThere(string nad)
+    {
+        ThrowEx.IsNullOrEmpty("nad", nad);
+        ThrowEx.IsNotWindowsPathFormat("nad", nad);
+
+
+        if (Directory.Exists(nad))
+        {
+            return;
+        }
+
+        List<string> slozkyKVytvoreni = new List<string>
+{
+nad
+};
+
+        while (true)
+        {
+            nad = Path.GetDirectoryName(nad);
+
+            // TODO: Tady to nefunguje pro UWP/UAP apps protoze nemaji pristup k celemu disku. Zjistit co to je UWP/UAP/... a jak v nem ziskat/overit jakoukoliv slozku na disku
+            if (Directory.Exists(nad))
+            {
+                break;
+            }
+
+            string kopia = nad;
+            slozkyKVytvoreni.Add(kopia);
+        }
+
+        slozkyKVytvoreni.Reverse();
+        foreach (string item in slozkyKVytvoreni)
+        {
+            string folder = item;
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+    }
+
+
+    //    internal static Func<string, bool> IsWindowsPathFormat;
+    //    internal static Func<string, string> GetDirectoryName;
+    //    internal static Action<string> CreateDirectory;
+    //    internal static Func<string, bool> ExistsDirectory;
+    //    internal static Action<string> CreateUpfoldersPsysicallyUnlessThere;
+    //    internal static Action<string> CreateFoldersPsysicallyUnlessThere;
+
+    //    internal static string WithEndSlash(string v)
+    //    {
+    //        return WithEndSlash(ref v);
+    //    }
+
+    //    /// <summary>
+    //    ///     Usage: Exceptions.FileWasntFoundInDirectory
+    //    /// </summary>
+    //    /// <param name="v"></param>
+    //    /// <returns></returns>
+    //    internal static string WithEndSlash(ref string v)
+    //    {
+    //        if (v != string.Empty)
+    //        {
+    //            v = v.TrimEnd(AllChars.bs) + AllChars.bs;
+    //        }
+
+    //        FirstCharUpper(ref v);
+    //        return v;
+    //    }
+
+    //    internal static void FirstCharUpper(ref string nazevPP)
+    //    {
+    //        nazevPP = FirstCharUpper(nazevPP);
+    //    }
+
+    //    internal static string FirstCharUpper(string nazevPP)
+    //    {
+    //        if (nazevPP.Length == 1)
+    //        {
+    //            return nazevPP.ToUpper();
+    //        }
+
+    //        string sb = nazevPP.Substring(1);
+    //        return nazevPP[0].ToString().ToUpper() + sb;
+    //    }
+}
---
+++ _sunamo/SunamoStringParts/SHParts.cs
@@ -0,0 +1,16 @@
+namespace SunamoPlatformUwpInterop;
+
+internal class SHParts
+{
+    internal static string RemoveAfterFirst(string t, string ch)
+    {
+        int dex = t.IndexOf(ch);
+        if (dex == -1 || dex == t.Length - 1)
+        {
+            return t;
+        }
+
+        string vr = t.Remove(dex);
+        return vr;
+    }
+}
