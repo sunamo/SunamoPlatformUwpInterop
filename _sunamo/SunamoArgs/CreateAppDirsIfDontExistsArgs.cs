a9b7a11b05a336f59ffad903cb08b05eb197cf02
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:50 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoArgs/CreateAppDirsIfDontExistsArgs.cs
@@ -0,0 +1,22 @@
+namespace SunamoPlatformUwpInterop;
+
+
+/// <summary>
+/// Musejí být všechny init protože už dále nedělám žádné checky na null
+/// </summary>
+public class CreateAppDirsIfDontExistsArgs
+{
+    internal string AppName = "";
+    /// <summary>
+    /// override
+    /// </summary>
+    internal List<string> keysCommonSettings = new List<string>();
+    /// <summary>
+    /// vylepšení pro non uwp apps
+    /// </summary>
+    internal List<string> keysSettingsList = new List<string>();
+    internal List<string> keysSettingsBool = new List<string>();
+    internal List<string> keysSettingsOther = new List<string>();
+    internal Func<List<byte>, List<byte>> RijndaelBytesDecrypt;
+    internal Func<List<byte>, List<byte>> RijndaelBytesEncrypt;
+}
