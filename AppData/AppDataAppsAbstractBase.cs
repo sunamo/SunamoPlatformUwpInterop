2e544307212c46fb823dea39c4958556d99b057f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 15:42:49 +0200

    All buildable, merged partial classes

--- AppData/AppDataAppsAbstractBase.cs
+++ AppData/AppDataAppsAbstractBase.cs
@@ -1,7 +1,27 @@
 namespace SunamoPlatformUwpInterop;
 
-public abstract partial class AppDataAppsAbstractBase<StorageFolder, StorageFile> : AppDataBase<StorageFolder, StorageFile>
+public abstract class AppDataAppsAbstractBase<StorageFolder, StorageFile> : AppDataBase<StorageFolder, StorageFile>
 {
+    public abstract StorageFolder GetRootFolder();
+
+    protected abstract void SaveFile(string content, StorageFile sf);
+
+
+
+
+
+    /// <summary>
+    /// Pokud rootFolder bude SE nebo null, G false, jinak vrátí zda rootFolder existuej ve FS
+    /// </summary>
+    public abstract bool IsRootFolderOk();
+    public abstract
+#if ASYNC
+        Task
+#else
+void  
+#endif
+        AppendAllText(string value, StorageFile file);
+
     /// <summary>
     /// AppDataAppsAbstractBase (this) - methods which are applied only on UAP
     /// AppDataAbstractBase  - methods which are applied only on desktop
--- SpecialFoldersHelper.cs
+++ SpecialFoldersHelper.cs
@@ -1,8 +1,39 @@
 namespace SunamoPlatformUwpInterop;
 
-
-public partial class SpecialFoldersHelper //: SpecialFoldersHelperSE
+public class SpecialFoldersHelper
 {
+    // public static bool aspnet
+    // {
+    //     get
+    //     {
+    //         return Exc.aspnet;
+    //     }
+    //     set
+    //     {
+    //         Exc.aspnet /*= SunamoExceptions.Exc.aspnet*/ = value;
+    //     }
+    // }
+
+    public static string AppDataRoaming()
+    {
+        string vr = null;
+
+        //if (Exc.aspnet || VpsHelperXlf.IsVps)
+        //{
+        // Create junction to Administrator
+        vr = @"C:\Users\Administrator\AppData\Roaming";
+        //}
+        //else
+        //{
+        //    var n = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        //    // Vracelo mi to empty string  s Environment.GetFolderPath
+        //    //vr = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        //    vr = @"C:\Users\"+ Path.GetFileName(n) + @"\AppData\Roaming";
+        //}
+
+        return vr;
+    }
+
     /// <summary>
     /// Return root folder of AppData (as C:\Users\n\AppData\)
     /// </summary>
