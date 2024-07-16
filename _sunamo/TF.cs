ec6ecbbd6ba130829282055bcbbbc6ae31d40941
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 16:37:42 +0200

    Before moving and deleting in _AddedToAllCsproj

--- _sunamo/TF.cs
+++ _sunamo/TF.cs
@@ -1,34 +1,34 @@
-namespace SunamoPlatformUwpInterop._sunamo;
 
+namespace SunamoPlatformUwpInterop._sunamo;
 //namespace SunamoPlatformUwpInterop._sunamo;
-//public class TF
+//internal class TF
 //{
-//    public static string ReadAllTextSync(string p)
+//    internal static string ReadAllTextSync(string p)
 //    {
 //        return File.ReadAllText(p);
 //    }
 
-//    public static List<byte> ReadAllBytesSync(string p)
+//    internal static List<byte> ReadAllBytesSync(string p)
 //    {
 //        return File.ReadAllBytes(p).ToList();
 //    }
 
-//    public static List<string> ReadAllLinesSync(string p)
+//    internal static List<string> ReadAllLinesSync(string p)
 //    {
 //        return File.ReadAllLines(p).ToList();
 //    }
 
-//    public static void WriteAllTextSync(string p, List<string> c)
+//    internal static void WriteAllTextSync(string p, List<string> c)
 //    {
 //        return File.ReadAllText(p);
 //    }
 
-//    public static List<byte> WriteAllBytesSync(string p, List<byte> c)
+//    internal static List<byte> WriteAllBytesSync(string p, List<byte> c)
 //    {
 //        return File.ReadAllBytes(p).ToList();
 //    }
 
-//    public static List<string> WriteAllLinesSync(string p, List<string> c)
+//    internal static List<string> WriteAllLinesSync(string p, List<string> c)
 //    {
 //        return File.ReadAllLines(p).ToList();
 //    }
